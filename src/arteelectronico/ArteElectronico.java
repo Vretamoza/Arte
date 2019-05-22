4cf2eb5924d1cb54aa8fb9caa4b508df81a18efa
Ivan Perez <ivanestebanperez@hotmail.com>
2019-05-21 21:01:31 -0500

    Borrar mensaje jjajaja

--- src/arteelectronico/ArteElectronico.java
+++ src/arteelectronico/ArteElectronico.java
@@ -15,7 +15,6 @@ public class ArteElectronico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Valery :)");
         Texto tx = new Texto();
         tx.setVisible(true);
     }
