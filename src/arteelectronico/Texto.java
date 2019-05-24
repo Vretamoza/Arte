2356f78349ba73a034950724ee52bc46f15d69c9
Ivan Perez <ivanestebanperez@hotmail.com>
2019-05-23 19:00:22 -0500

    Savepoint, Agrege la historia

--- src/arteelectronico/Texto.java
+++ src/arteelectronico/Texto.java
@@ -1,5 +1,8 @@
-package arteelectronico;
 
+
+package arteelectronico;
+import javax.swing.JOptionPane;
+import java.util.Random;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,6 +23,25 @@ public class Texto extends javax.swing.JFrame {
         o4.setVisible(false);
         o5.setVisible(false);
         o6.setVisible(false);
+        String Opciones1[] = {"\"Cuando encuentra el apartamento que quería, empieza \\na llamar al camión de la mudanza, esperando en la acera \\na que el camión llegue se encuentre con un indigente que \\nle pide dinero ella lo observa y le da 4 mil pesos porque \\nsabe que ese hombre debe de estar pasando hambre; muy a su\\npesar el hombre se levanta se mete a la tienda que queda\\nenfrente de su casa a comprar alcohol y emborracharse. \\n\"\n" +
+"                + \"El camión de mudanza nunca llega, llegaría dentro de \\nun día , porque los hombres se perdieron por \\nel camino peligroso y nada transitado. \\n\"\n" +
+"                "
+                + "+ \"Al día siguiente tiene que llevar ropa que ya ha usado \\nporque la mudanza nada que llegaba a sus clases \\nregulares y para cuando vuelve el camión ya está esperándola \\ny comienza a descargar sus cosas. \\n\";",
+                 
+            "Luego de elegir el apartamento que quiere, llama al \ncamión de la mudanza y mientras espera ve a un indigente que \nle pide dinero, ella en cambio le da comida, porque sabe \nque ese hombre debe de tener hambre, el hombre la mira con \nojos tiernos y devora la comida que la chica le dió. \n"
+                + "El camión de la mudanza llega y ella empieza a descargar \nsus cosas. "};
+        
+        String Opciones2[] = {"La chica elige sushi y espera su pedido, cuando llega \nella lo devora sin dejar nada, pero lastimosamente se \ndespierta a medianoche a vomitar porque el pescado crudo le cayó mal. \nY se verá obligada a estar yendo al baño cada cierto \ntiempo, pero no puede faltar a clases porque tiene un \nquiz muy importante.",
+            "La chica elige pizza y espera su pedido, cuando llega \nse come la mitad y deja algunos pedazos para que desayune \nal día siguiente."};
+        
+        String Opciones3[] = {"\"Le pregunta qué le pasa, él le cuenta que está triste porque \\ quiere dejar el alcohol pero se le hace muy difícil \\ ella decide aconsejarle que se meta en \\ un grupo de alcohólicos anónimos y reduzca las dosis de alcohol poco a poco \\ no lo suspenda tan bruscamente porque la ansiedad que sentirán sera peor \\ el le agradece sus consejos le da las botellas de cerveza que tiene escondidas \\ y le pide que las arroje a la basura,\\ solo se quedaría con una, ella hace y le desea buenas noches a Jaime \\ y sube a su apartamento.\n" +
+"\"A la mañana siguiente cuando ella se dirige a la universidad se encuentra a Jaime \\ y este le cuenta que ya se inscribió a un grupo de alcohólicos y hoy es su primera cita,\\ ella le dice que si quiere después de que llegue de la universidad \\ puede ir a asearse a su apartamento para ir bien presentado,\\ a pesar de no ser una entrevista él quiere hacer lo mejor \\ porque es algo supremamente importante para el, Jaime acepta contento la oferta de la chica. \n" +
+"\"jaime va a su reunión y se siente muy bien, \\ decide ir a contarle a la chica y ella lo felicita Jaime \\ está muy feliz por su avance.",
+            
+            "\"hacen contacto visual y sigue su camino hasta llegar a su apartamento\\ pero los sollozos de Jaime no la dejan dormir.\n" +
+"\"a la mañana siguiente ve a Jaime con los ojos rojos de tanto llorar \\ y ella igual porque no pudo pegar ojo en toda la noche por los sollozos de él \\ se siente fatal por no haberlo ayudado, pero ella no le debe nada,\\  decide seguir de largo e irse a la universidad.\\ cuando ella vuelve de la universidad encuentra a Jaime borracho lleno de basura.\\ decide no mirar y sube a su apartamento, escucha golpes, Jaime está recibiendo una paliza,\\ lo dejan tendido en el piso, ella solo puede mirar, no puede hacer más nada, \\ al fin de cuentas él no es su papá"};
+       
+        String Opciones4[] = {"",""};
     }
 
     /**
@@ -266,6 +288,26 @@ public class Texto extends javax.swing.JFrame {
             }
         });
     }
+    public static void Random(int Opciones[]){
+        Random Generar = new Random();
+        for(int i =1;i<=2;i++){
+            int Aux = Generar.nextInt(2)+1;
+            if(!comprobarNumero(Aux,Opciones)){
+                Opciones[i]=Aux;
+            }
+            else{
+                i--;
+            }
+        }
+    }
+    public static boolean comprobarNumero(int num,int Al[]){
+               for(int i=1;i<=3;i++){
+                       if(Al[i]==num){
+                            return true;
+                       }
+               }
+               return false;
+         }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextArea area;
