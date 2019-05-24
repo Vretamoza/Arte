4f8e7d518eaf7d0945293dff1b5fd14e9942a888
Ivan Perez <ivanestebanperez@hotmail.com>
2019-05-23 23:14:17 -0500

    Cambios en Test +100 lineas de codigo

--- src/arteelectronico/Test2.java
+++ src/arteelectronico/Test2.java
@@ -17,6 +17,12 @@ public class Test2 extends javax.swing.JFrame {
     /**
      * Creates new form Texto
      */
+    public int Sel[]=new int [3];
+    public int SelO=1;
+    public int SelO1=1;
+    public int SelEsp;
+    public int SelO2=1;
+    public boolean allow=false;
     public String Opciones1[] = {"\"Cuando encuentra el apartamento que quería, empieza \\na llamar al camión de la mudanza, esperando en la acera \\na que el camión llegue se encuentre con un indigente que \\nle pide dinero ella lo observa y le da 4 mil pesos porque \\nsabe que ese hombre debe de estar pasando hambre; muy a su\\npesar el hombre se levanta se mete a la tienda que queda\\nenfrente de su casa a comprar alcohol y emborracharse. \\n\"\n" +
 "                               + \"El camión de mudanza nunca llega, llegaría dentro de \\nun día , porque los hombres se perdieron por \\nel camino peligroso y nada transitado. \\n\"\n" +
 "                "
@@ -41,6 +47,7 @@ public class Test2 extends javax.swing.JFrame {
     
     public Test2() {
         initComponents();
+        Random(Sel);
     }
 
     /**
@@ -52,6 +59,7 @@ public class Test2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Next = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         Text = new javax.swing.JTextArea();
         Ing2 = new javax.swing.JButton();
@@ -59,7 +67,15 @@ public class Test2 extends javax.swing.JFrame {
         Background = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(null);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        Next.setText("Next");
+        Next.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NextActionPerformed(evt);
+            }
+        });
+        getContentPane().add(Next, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 380, 110, 50));
 
         Text.setColumns(20);
         Text.setFont(new java.awt.Font("Monospaced", 0, 18)); // NOI18N
@@ -67,8 +83,7 @@ public class Test2 extends javax.swing.JFrame {
         Text.setText("Una chica que está en segundo año \nde derecho ha vuelto a la ciudad \ndonde creció después de haber hecho \nsu primer año en otro país. Está buscando \napartamentos que queden cerca de su \nuniversidad para que todo sea más fácil. \nEncuentra dos apartamentos \n1. 5 minutos de la universidad pero por un camino \ntotalmente solo y peligroso \n2. 20 minutos de la universidad pero por un camino \nmás transitado, pero al ser más transitado ocurren \nmuchos trancones. \n");
         jScrollPane1.setViewportView(Text);
 
-        getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(10, 20, 726, 254);
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 726, 254));
 
         Ing2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Webp.net-resizeimage.png"))); // NOI18N
         Ing2.setText("jButton1");
@@ -79,8 +94,7 @@ public class Test2 extends javax.swing.JFrame {
                 Ing2ActionPerformed(evt);
             }
         });
-        getContentPane().add(Ing2);
-        Ing2.setBounds(490, 330, 112, 145);
+        getContentPane().add(Ing2, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 330, 112, 145));
 
         Ing1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Webp.net-resizeimage.png"))); // NOI18N
         Ing1.setText("jButton1");
@@ -91,24 +105,139 @@ public class Test2 extends javax.swing.JFrame {
                 Ing1ActionPerformed(evt);
             }
         });
-        getContentPane().add(Ing1);
-        Ing1.setBounds(80, 330, 110, 145);
+        getContentPane().add(Ing1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 330, 110, 145));
 
         Background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/A_blank_black_picture.jpg"))); // NOI18N
         Background.setText("jLabel2");
-        getContentPane().add(Background);
-        Background.setBounds(0, 0, 740, 510);
+        getContentPane().add(Background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 740, 510));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void Ing1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Ing1ActionPerformed
+       
+        if(SelO==1){
+            Text.setText(Opciones1[Sel[1]]);
+            for(int i=1;i<=3;i++){
+                Sel[i]=0;
+            }
+            Random(Sel);
+        }
+        if(SelO==2){
+            
+        }
+        if(SelO==3){
+            
+        }
+        if(SelO==4){
+            
+        }
         
     }//GEN-LAST:event_Ing1ActionPerformed
 
     private void Ing2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Ing2ActionPerformed
-        // TODO add your handling code here:
+
+        if(SelO==1){
+            Text.setText(Opciones1[Sel[2]]);
+            for(int i=1;i<=3;i++){
+                Sel[i]=0;
+            }
+            Random(Sel);
+            allow=true;
+        }
+        if(SelO==2){
+            
+        }
+        if(SelO==3){
+            
+        }
+        if(SelO==4){
+            
+        }
     }//GEN-LAST:event_Ing2ActionPerformed
+
+    private void NextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NextActionPerformed
+        if(allow==true){
+            if(SelO==1){
+                Text.setText("Cuando ya ha terminado de acomodar todo, cuyo procesos le llevó 3 días está cansada y hambrienta decide ordenar comida por rappi, tiene 15k en rappicreditos si come sushi le saldrá totalmente gratis, pero si coge pizza tendrá que pagar 3k demás.");
+                SelO++;
+                allow=false;
+            }
+            if(SelO==2){
+                if(SelO1==1){
+                    Text.setText("A la mañana siguiente se va a la universidad y se encuentra al indigente este la saluda alegremente, hay algo en el que le recuerda a alguien pero no sabe a quien, cuando va caminando alguien grita su apellido y ella y el indigente voltean al mismo tiempo, ambos se miran extrañados.");
+                    SelO1++;
+                }
+                if(SelO1==2){
+                    Text.setText("Señor: ¿Es usted apellido Perez señorita? Hola, mi nombre es Jaime Perez\n" +
+                            "Ella sorprendida se da cuenta que el es su padre, el que la abandonó cuando era muy pequeña porque el tenia  problemas con el alcohol.\n" +
+                            "Chica: Hola, mi nombre es Julia Pérez (miente)\n" +
+                            "Señor: ¡vaya! tenemos el mismo apellido, que coincidencia");
+                    SelO1++;
+                }
+                if(SelO1==3){
+                    Text.setText("la chica se despide del hombre y se dirige al paradero de buses, ella pensó que sentiría rabia al ver al hombre que le dio la vida pero que la abandonó a ella y a su mama despues de tantos años, pero sorprendentemente siente mucha pena por él, en el estado en el que está, se dejó consumir por el alcohol.\n" +
+                             "la chica va a clase regular y cuando llega al edificio, encuentra a Jaime que está llorando.");
+                    SelO1=1;
+                    allow=false;
+                }
+            }
+            if(SelO==3){
+                if(SelO1==1){
+                Text.setText("Ya han pasado varios meses desde que jaime estuvo llorando, la chica y Jaime han hablado más los últimos meses, escucho de los vecino que tuvo una hija, pero que la abandonó y se arrepiente de ella cada dia pero no puede hacer nada ahora mismo.");
+                SelO1++;
+            }
+            if(SelO1==2){
+                Text.setText("la chica se siente un poco mal al escuchar eso, todavía no sabe si decirle que ella es su hija, pero la verdad no quiere sentir que él al saber eso va a estar con ella por compromiso o porque realmente la ama y la extrañó después de tantos años, pero no quiere averiguarlo, simplemente no le dirá.");
+                SelO1=1;
+                allow=false;
+            }
+                
+            }
+            if(SelO==4){
+                if(SelEsp==1){
+                    Text.setText("Jaime al estar sobrio empieza a recordar y le cuenta a la chica que está arrepentido de haber dejado perder el control de su vida por causa del alcohol, y le dice que le gustaría poder reencontrarse con su familia y pedirles perdón por haberles abandonado. Y que además ahora que consiguió un trabajo, puede ayudarles con los gastos");
+            }
+                if(SelEsp==2){
+                    if(SelO2==1){
+                        Text.setText("Después de varios días la chica no ha vuelto a ver al indigente que era su padre. Al salir hacia la universidad se encuentra con una de sus vecinas y caminan juntas a tomar el bus. ");
+                        SelO2++;
+                    }
+                    if(SelO2==2){
+                        Text.setText("De camino a la parada de bus pasan por la esquina en la que siempre estaba Jaime y su vecina le dice que desde que internaron a Jaime ahora el barrio se veía limpio y no lleno de la basura con la que este convive");
+                        SelO2++;
+                    }
+                    if(SelO2==3){
+                        Text.setText("La chica al escuchar esto queda en shock y le pregunta a su vecina en cual hospital lo tienen internado, ella le responde y luego la chica le pide que se adelante, que olvidó algo en casa");
+                        SelO2++;
+                    }
+                    if(SelO2==4){
+                        Text.setText("La chica corre a su casa y se encierra a llorar, porque a pesar de no querer entablar una relación con su padre, sigue siendo su padre, sale del edificio, toma un taxi y llega al hospital, pregunta por Jaime Pérez y la enferma le cuenta que el necesita un trasplante de hígado o morirá");
+                        SelO2++;
+                    }
+                    if(SelO2==5){
+                        Text.setText("para que un alcohólico reciba una donación debe de tener por lo menos 1 año de sobriedad y Jaime no cumple con esa característica, así que no califica en la lista");
+                    }
+                    if(SelO2==6){
+                        Text.setText("la única manera de recibir el higado es que un familiar donde por el, al escuchar eso, la chica le dice a la enferma que ella es su hija y se quiere hacer los exámenes de compatibilidad para ver si es apta para donar o no");
+                    }
+                    if(SelO2==7){
+                        Text.setText("luego de una larga espera, los exámenes salen positivos; la chica va corriendo a la habitación de su papá y le dice que encontraron un donante para él y ");
+                    }
+                    if(SelO2==8){
+                        Text.setText("el sorprendido dice que no lo puede creer que el no esta en la lista pero la chica le dice que es ella quien va a donarle un pedazo de su hígado");
+                    }
+                    if(SelO2==8){
+                        Text.setText("y le dice que queda en él la decision, si lo acepta o no, le dice su verdadero nombre, monica perez y cae en la cuenta de que ella es su hija.");
+                    }
+                    if(SelO2==8){
+                        Text.setText("");
+                    }
+                    
+            }
+            }
+        }
+    }//GEN-LAST:event_NextActionPerformed
 
     /**
      * @param args the command line arguments
@@ -157,7 +286,7 @@ public class Test2 extends javax.swing.JFrame {
         }
     }
     public static boolean comprobarNumero(int num,int Al[]){
-               for(int i=1;i<=3;i++){
+               for(int i=1;i<=2;i++){
                        if(Al[i]==num){
                             return true;
                        }
@@ -169,6 +298,7 @@ public class Test2 extends javax.swing.JFrame {
     private javax.swing.JLabel Background;
     private javax.swing.JButton Ing1;
     private javax.swing.JButton Ing2;
+    private javax.swing.JButton Next;
     private javax.swing.JTextArea Text;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
